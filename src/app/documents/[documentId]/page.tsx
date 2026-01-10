5e0a25569ddeba56d3ce6caec86fbbeb96eb3273
raj-prasan <rajprasanj@gmail.com>
2026-01-10 20:16:58 +0530

    initial commit

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,16 @@
+import {Editor} from "./editor"
+
+interface DocumentIdPageProps {
+  params: Promise<{documentId: string}>
+}
+
+const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
+  const documentId =  (await params).documentId
+  return ( 
+  <div className="min-h-screen bg-[#FAFBFD]">
+    
+    <Editor></Editor>
+  </div> );
+}
+ 
+export default DocumentIdPage;
